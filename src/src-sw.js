c5f7330c4c405309983db7f52f96decc4d006f00
Trent Apted <tapted@gmail.com>
2018-12-03 21:37:58 +1100

    stalewhilerevalidate

--- src/src-sw.js
+++ src/src-sw.js
@@ -1,1 +1,13 @@
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+const thumbRegex = new RegExp('=w[0-9]*-h[0-9]*-c$');
+const matchThumb = ({url, event}) => {
+  if (!url.hostname.endswith('googleusercontent.com'))
+    return false;
+  return thumbRegex.test(url.pathname);
+};
+
+workbox.routing.registerRoute(
+  matchThumb,
+  workbox.strategies.staleWhileRevalidate(),
+);
